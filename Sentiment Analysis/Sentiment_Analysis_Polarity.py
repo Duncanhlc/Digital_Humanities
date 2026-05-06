4045e00f14ea6db6557f1e304fbb53eb234b3388
Duncanhlc <66673636+Duncanhlc@users.noreply.github.com>
2026-05-06 15:22:26 +0800

    Add files via upload

---
+++ Sentiment Analysis/Sentiment_Analysis_Polarity.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+# Load the data
+df = pd.read_csv('../Text_Processed.csv')
+
+# Convert Serial to Date (1 = 1831-12-12)
+start_date = pd.to_datetime('1831/12/12')
+df['Date'] = start_date + pd.to_timedelta(df['Serial'] - 1, unit='D')
+
+# Remove 0 values
+df['Polarity'] = df['Polarity'].replace(0, None)
+
+# Plot
+fig, ax = plt.subplots(figsize=(12.8, 7.2), dpi=100)
+ax.plot(df['Date'], df['Polarity'], label='Polarity', color='blue')
+
+# Title + label
+plt.title("Polarity Over Time (1831–1836)")
+
+ax.xaxis.set_major_locator(mdates.MonthLocator(bymonth=(1, 7)))
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y/%m/%d'))
+plt.xticks(rotation=45)
+
+plt.ylabel("Score (-1 to 1)")
+
+# limit
+plt.ylim((-1, 1))
+
+# Other
+plt.legend()
+plt.grid()
+plt.tight_layout()
+
+# Save the plot
+plt.savefig('Polarity.png')
+plt.show()
